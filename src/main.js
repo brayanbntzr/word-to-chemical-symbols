3e7e6b8c486d336e2bdfe58ebb9b3765c7138d7e
brayanbntzr <brayanbntzrsde@gmail.com>
2023-08-26 22:48:01 -0600

    main algorithm added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
-
+// Read Json file
 const req = await fetch('../elements.json');
-const elements = await req.json();
+const Elements = await req.json();
 
-console.log(elements);
+// Finds if a string can be written with symbols
+const convert = (s_input) => {
+	let n = s_input.length;
+	if(n == 0) return "";
+
+	for (let i = 0; i < Elements.length; i++) {
+		let element = Elements[i];
+		let symbol = element.Symbol.toLowerCase();
+		let m = symbol.length;
+		if(symbol.length <= n){
+			let str = s_input.substring(0, m);
+			if(symbol === str){
+				if(n === m){
+					return element.Symbol;
+				}else{
+					let res = convert(s_input.substring(m));
+					if(res !== ""){
+						return element.Symbol + res;
+					}
+				}
+			}
+		} 
+	}
+
+	return "";
+}
